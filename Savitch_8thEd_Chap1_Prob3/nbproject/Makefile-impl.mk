5fa75d14511d5b6cf923067bc0a59053e32fdec0
Shen Jin <jinshen_7@yahoo.com>
2014-06-25 01:03:01 -0700

    Signed-off-by: Shen Jin <jinshen_7@yahoo.com>

--- Savitch_8thEd_Chap1_Prob3/nbproject/Makefile-impl.mk
+++ Savitch_8thEd_Chap1_Prob3/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Savitch_8thEd_Chap1_Prob2
+PROJECTNAME=Savitch_8thEd_Chap1_Prob3
 
 # Active Configuration
 DEFAULTCONF=Debug
